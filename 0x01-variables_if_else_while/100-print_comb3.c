29783dd426daeaa6260ad9d06eb01ce578a023b3
amarachi oji <99545109+Theamarachi@users.noreply.github.com>
2022-03-12 05:33:16 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
- int tens;
- int ones;
+int tens;
+int ones;
 
- for (tens = 0; tens <= 9; tens++)
- {
- for (ones = tens + 1; ones <= 9; ones++)
+for (tens = 0; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
 {
 putchar(tens + '0');
 putchar(ones + '0');
