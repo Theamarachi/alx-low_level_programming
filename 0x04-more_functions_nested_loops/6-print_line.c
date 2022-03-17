9930accf345636bdfd5d8cd972abd1893d4ba859
amarachi oji <99545109+Theamarachi@users.noreply.github.com>
2022-03-17 19:41:17 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_line - draws a straight line in the terminal.
+*@n: number of '_' to be printed.
+* Return: Void.
+*/
+void print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
