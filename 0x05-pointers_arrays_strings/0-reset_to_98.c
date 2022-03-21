ab8896dc602b7351c6fa2b85643fa8073e677adb
amarachi oji <99545109+Theamarachi@users.noreply.github.com>
2022-03-21 21:07:51 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *reset_to_98 - resets value in address referenced by pointer.
- *@n: pointer.
- *
- *Return: void.
+ * reset_to_98 - this functions takes a pointer to an int
+ * @n: puntero
+ * Return: (0)
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
