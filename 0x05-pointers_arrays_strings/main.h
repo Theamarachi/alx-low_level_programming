50e6c345c8a6bf01f753e66c5ae92fb50de3efe8
amarachi oji <99545109+Theamarachi@users.noreply.github.com>
2022-03-21 21:10:56 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,4 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar();
 
-#endif /* MAIN_H */
+#endif
