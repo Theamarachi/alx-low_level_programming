1c05a895241397549f217fd909f8ebd98c59883e
amarachi oji <99545109+Theamarachi@users.noreply.github.com>
2022-03-28 23:26:16 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strchr - locates a character in a string
  * @s: the string
@@ -10,7 +9,6 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-
 	for (i = 0; s[i] != '\0' ; i++)
 	{
 		if (s[i] == c)
